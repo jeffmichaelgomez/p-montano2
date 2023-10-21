d7b27f8cc6d167509018cb17ef9a809d276e720f
jeffmichaelgomez <jeffmichaelgomez@gmail.com>
2023-10-22 03:10:14 +0800

    feat: initial commit

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useLayoutEffect, useRef } from 'react'
+import styles from './style.module.css';
+import Image from 'next/image';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+const projects = [
+    {
+        title: "Portfolio",
+        src: "1st.png"
+    },
+    {
+        title: "Residential",
+        src: "2nd.png"
+    },
+    {
+        title: "3D Isometric View",
+        src: "3rd.png"
+    },
+    {
+        title: "Elevations",
+        src: "4th.png"
+    },
+]
+
+export default function Index() {
+
+    const [selectedProject, setSelectedProject] = useState(0);
+    const container = useRef(null);
+    const imageContainer = useRef(null);
+
+    useLayoutEffect( () => {
+        gsap.registerPlugin(ScrollTrigger);
+        ScrollTrigger.create({
+            trigger: imageContainer.current,
+            pin: true,
+            start: "top-=100px",
+            end: document.body.offsetHeight - window.innerHeight - 50,
+        })
+    }, [])
+
+    return (
+        <div ref={container} className={styles.projects}>
+            <div className={styles.projectDescription}>
+                <div ref={imageContainer} className={styles.imageContainer}>
+                    <Image 
+                        src={`/images/${projects[selectedProject].src}`}
+                        fill={true}
+                        alt="project image"
+                        priority={true}
+                    />
+                </div>
+                <div className={styles.column}>
+                    <p>Adding Dimensions to Dreams.</p>
+                </div>
+                <div className={styles.column}>
+            
+                </div>
+            </div>
+
+            <div className={styles.projectList}>
+                {
+                    projects.map( (project, index) => {
+                        return <div key={index} onMouseOver={() => {setSelectedProject(index)}} className={styles.projectEl}>
+                            <h2>{project.title}</h2>
+                        </div>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
